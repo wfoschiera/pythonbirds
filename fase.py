923d99dd86b1f61251f8fb398e1f62b3fa292c97
wfoschiera <wfoschiera@gmail.com>
2020-09-12 14:53:42 -0400

    implementado métodos de verificação do intervalo de de colisão padrão.

--- fase.py
+++ fase.py
@@ -108,8 +108,9 @@ class Fase():
         """
         for passaro in self._passaros:
             passaro.calcular_posicao(tempo)
-            for alvo in self._obstaculos+self._porcos:
+            for alvo in self._obstaculos + self._porcos:
                 passaro.colidir(alvo, self.intervalo_de_colisao)
+            passaro.colidir_com_chao()
         pontos=[self._transformar_em_ponto(a) for a in self._passaros+self._obstaculos+self._porcos]
 
         return pontos
